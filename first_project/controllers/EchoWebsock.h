a3d6faf83eed8ff74abb9fb561ebc1bc42cc01aa
HrushikeshAnandSarangi <hrushikeshsarangi7@gmail.com>
2025-10-22 06:59:33 +0000

    pausing the project

---
+++ first_project/controllers/EchoWebsock.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <drogon/WebSocketController.h>
+
+using namespace drogon;
+
+class EchoWebsock : public drogon::WebSocketController<EchoWebsock>
+{
+  public:
+     void handleNewMessage(const WebSocketConnectionPtr&,
+                                  std::string &&,
+                                  const WebSocketMessageType &) override;
+    void handleNewConnection(const HttpRequestPtr &,
+                                     const WebSocketConnectionPtr&) override;
+    void handleConnectionClosed(const WebSocketConnectionPtr&) override;
+    WS_PATH_LIST_BEGIN
+    // list path definitions here;
+    WS_PATH_ADD("/echo");
+    // WS_PATH_ADD("/path", "filter1", "filter2", ...);
+    WS_PATH_LIST_END
+};
--- first_project/main.cc
+++ first_project/main.cc
@@ -1,11 +1,13 @@
 #include <drogon/drogon.h>
 int main() {
     //Set HTTP listener address and port
-    drogon::app().addListener("0.0.0.0", 5555);
+    drogon::app().addListener("0.0.0.0", 8000);
     //Load config file
     //drogon::app().loadConfigFile("../config.json");
     //drogon::app().loadConfigFile("../config.yaml");
     //Run HTTP framework,the method will block in the internal event loop
+    LOG_INFO<<"Drogon Started on port 8000";
     drogon::app().run();
+    
     return 0;
 }
